d3d314862eb866f2e04740fd577dd30c25f9b98a
nickarafyllis <nickarafyllis@gmail.com>
2023-11-06 01:16:36 +0200

    ordering + bugs fixed

--- lab2/code.py
+++ lab2/code.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 import librosa 
 import os 
-import numpy as np
 import re 
 
 #step 2
+dir = "../patrec-files/pr_lab1/pr_lab1_2020-21_data/digits"
 dir = "../pr_lab1_2020-21_data/digits"
 
+#get  speaker number and digit of a file by splitting its name
 def file_split(filename):
     match = re.match(r'([A-Za-z]+)(\d+)', filename)
     text, number_part = match.groups()
     number = int(number_part)
     return number, text
 
+#get wav, speaker number and digit of every wav file in directory
 def data_parser(dir):
-    wavs = []
+    wav = []
     speaker = []
     digit = []
     wav = []
-    for num, filename in enumerate(os.listdir(dir)):
+    for filename in (os.listdir(dir)):
         path = os.path.join(dir, filename)
-        wavs.append(librosa.load(path, sr=16000))
-        wav = [item[0].tolist() for item in wavs]
+        wav.append(librosa.load(path, sr=16000))
         number, text = file_split(filename)
         digit.append(text)
         speaker.append(number)
-        break
+        
+    # order lists by digit and speaker number
+    sorted_data = sorted(zip(wav, speaker, digit), key=lambda x: (x[2], x[1])) 
+    wav, speaker, digit = zip(*sorted_data)
+    
     return wav, speaker, digit
 
 wav, speaker, digit = data_parser(dir)
 
-
+#test 
+# Print all elements in the first 5 positions of each list
+# print("First a wav:", wav[:1])
+# print("First 5 speakers:", speaker[:5])
+# print("First 5 digits:", digit[:5])
